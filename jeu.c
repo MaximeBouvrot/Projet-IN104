9c1d94f2992e0bfb900be405af1f11bcd3909581
Mahé <m.hersart@gmail.com>
2022-04-12 15:21:57 +0200

    changement du main

--- jeu.c
+++ jeu.c
@@ -3,37 +3,6 @@
 #include <time.h>
 
 
-int nombre_de_mot(FILE* texte){
-    int nb_lignes = 0 ;
-    fscanf(texte);
-    while (texte != feof){
-        fscanf(texte);
-        nb_lignes ++ ;
-    }
-
-}
-
-int nombrealea(int n){
-    srand(time(NULL));
-    return (rand() % n);
-}
-
-
-char* choisir_un_mot_alea(FILE* dico ){
-    int n = nombre_de_mot(dico);
-    int aleat = nombrealea(n);
-    while(n>0){
-        fscanf(dico);
-    }
-    char* mot_alea;
-    fscanf(dico,"%s",&mot_alea);
-    return mot_alea ;
-}
-
-
-
-
-
 
 int main(){
 

9cab1f02627f35eacd51b73a231fe2b0726dca97
MAXIME <bouvrot.maxime@orange.fr>
2022-04-19 14:57:34 +0200

    ajout de fonction dico.h

---
+++ fonction_dico.h
@@ -0,0 +1,11 @@
+#ifndef __FONCTION_DICO_H__
+#define __FONCTION_DICO_H__
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+int nombre_de_mot(FILE* texte);
+char* choisir_un_mot_alea(FILE* dico );
+
+#endif

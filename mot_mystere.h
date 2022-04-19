27ce71da5b4d42ee22e008e35a5af4347d3afe93
MAXIME <bouvrot.maxime@orange.fr>
2022-04-19 16:37:56 +0200

    flk,qeof

---
+++ mot_mystere.h
@@ -0,0 +1,12 @@
+#ifndef __MOT_MYSTERE_H__
+#define __MOT_MYSTERE_H__
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+int nombre_de_mot(FILE* texte);
+int nombrealea(int n);
+void choisir_un_mot_alea(int n, char* mot )
+
+#endif

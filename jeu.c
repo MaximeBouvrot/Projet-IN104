869ecc9ee4ce879d15bbc4f7223887d74aeaf480
MAXIME <bouvrot.maxime@orange.fr>
2022-04-19 15:11:31 +0200

    fin de la vérification de mot, ajout des différentes étapes et de la fin

--- jeu.c
+++ jeu.c
@@ -18,14 +18,35 @@ int main(){
         return 0;
     }
 
-        choisir_un_mot_alea(dico);
+    // On choisit un mot de 5 lettres dans le dictionnaire
+        mot_mystere=choisir_un_mot_alea(dico);
 
 
+    //On demande un mot à l'utilisateur et on vérifie que son mot est valide (5 lettre et  existe ds le dico)
     char* mot_utilisateur=malloc (sizeof(char));
     scanf("%s",mot_utilisateur);
     while (! mot_valide(mot_utilisateur, dico)){
         scanf("%s",mot_utilisateur);
         }
+    
+    //On compare le mot de l'utilisateur et le mot mystère
+	int i=1;
+	while (strcmp(mot_utilisateur,mot_mystere)!=0 || i<6){
+        //On regarde les différences et on les liste
 
+        //On fait un affichage qui permet à l'utilisateur de savoir les positions/ce qui est faux
+
+    i+=1;
+    }
+
+    if (strcmp(mot_utilisateur,mot_mystere)!=0){
+        printf("Perdu, relance le jeu pour réessayer")
+        return 0
+    }
+
+    else {
+        printf("Bravo, félicitations c'était bien %s",mot_mystere )
+        return 0
+    }
 }
 

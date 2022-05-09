00fbe6b99bb4e3094b69cd44e72a314abdfb5142
MAXIME <bouvrot.maxime@orange.fr>
2022-05-09 17:58:38 +0200

    ajout du fichier comparaison.h

---
+++ comparaison.h
@@ -0,0 +1,12 @@
+#ifndef __COMPARAISON_H__
+#define __COMPARAISON_H__
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <string.h>
+
+int cherche(char *s, char carac);
+int comparaison(char*mot_utilisateur, char*mot);
+
+#endif

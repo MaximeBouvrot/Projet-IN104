e8476fb71fa2b9aa7b8fea948a72d1b5b3e4e5ae
Mahé <m.hersart@gmail.com>
2022-05-10 15:51:14 +0200

    jeu.c

--- jeu.c
+++ jeu.c
@@ -24,12 +24,12 @@ int main(void){
     // On choisit un mot de 5 lettres dans le dictionnaire
     int n = nombre_de_mot(dico);
     int p = nombrealea(n);
-    char* mot = malloc(sizeof(char));
-    choisir_un_mot_alea(p, mot);
+    char* mot = malloc(26*sizeof(char));
+    choisir_un_mot_alea(p, mot, dico);
     printf("nombre de lignes : %d\nnombre aléatoire : %d\nmot mystère : %s\n",n,p,mot);
 
     //Comparaison des mots utilisateur et du mot mystère :
-    rewind(dico);
+
     printf("veuillez saisir un mot de 5 lettres\n");
 	int i=1;
     char* mot_utilisateur=malloc(26*sizeof(char));
@@ -43,9 +43,6 @@ int main(void){
 
     }
 
-    free(mot_utilisateur);
-    fclose(dico);
-
     //Affichage des résultats :
 
     if (strcmp(mot_utilisateur,mot)!=0){
@@ -57,5 +54,7 @@ int main(void){
         printf("Bravo, félicitations c'était bien %s, tu l'as trouvé en %d tentatives\n",mot,i );
         return 0;
     }
+    free(mot_utilisateur);
+    fclose(dico);
 }
 
